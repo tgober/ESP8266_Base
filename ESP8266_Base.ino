b6577caf2d121232ebcca8c2f8ab113e9e61edb0
Sebastian Janzen <sebastian@janzen.it>
2015-12-13 14:03:37 +0100

    Handle POST requests at /wlanSetup

--- ESP8266_Base.ino
+++ ESP8266_Base.ino
@@ -53,7 +53,7 @@ void handleRoot()
 
 void handleWlanSetup()
 {
-  server.send(200, "text/html", "<!doctype html><body><head></head><html><h1>You are connected</h1><form method=\"POST\" action=\"/wlanSetupConfirm\"><label id=\"ssid\">SSID</label><input id=\"ssid\" name=\"ssid\"><br><label id=\"pwd\">pwd</label><input id=\"pwd\" name=\"password\"><br><input type=\"submit\" value=\"Update\"></form></body></html>");
+  server.send(200, "text/html", "<!doctype html><body><head></head><html><h1>You are connected</h1><form method=\"POST\" action=\"/wlanSetup\"><label id=\"ssid\">SSID</label><input id=\"ssid\" name=\"ssid\"><br><label id=\"pwd\">pwd</label><input id=\"pwd\" name=\"password\"><br><input type=\"submit\" value=\"Update\"></form></body></html>");
 }
 
 void handlePwdPost()
@@ -217,8 +217,8 @@ void setup() {
   getEepromContent();
 
   server.on("/", handleRoot);
-  server.on("/wlanSetup", handleWlanSetup);
-  server.on("/wlanSetupConfirm", HTTP_POST, handlePwdPost);
+  server.on("/wlanSetup", HTTP_GET, handleWlanSetup);
+  server.on("/wlanSetup", HTTP_POST, handlePwdPost);
   server.on("/setValue", handlePostValue);
   
   Serial.println("HTTP server started");
